e826b6f5f3c620bba7c9499256dda09663bdfd93
Glenn Goodrich <glenn.goodrich@gmail.com>
2015-04-10 13:17:14 -0400

    Correct partial path in override

--- app/overrides/add_price_categories_tab.rb
+++ app/overrides/add_price_categories_tab.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => 'spree/admin/shared/_configuration_menu',
+Deface::Override.new(:virtual_path => 'spree/admin/shared/sub_menu/_configuration',
 	:name => 'add_price_categories_tab',
 	:insert_bottom => '[data-hook="admin_configurations_sidebar_menu"]',
 	:text => '<%= configurations_sidebar_menu_item Spree.t(:price_categories), admin_price_categories_path %>',
